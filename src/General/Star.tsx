6a0b01e4be5ca9a22f782de044db690169517d21
Zhassulan Baigozha <zhassulan.baigozha@nitec.kz>
2024-11-20 11:23:44 +0500

    fix: [egov-8808] icon Star props

--- src/General/Star.tsx
+++ src/General/Star.tsx
@@ -1,31 +1,23 @@
 import React from "react";
-import { iconBaseDefaults } from "../constants/iconDefaults";
 
-export const Star = ({
-  fill = iconBaseDefaults.fill,
-  width = iconBaseDefaults.width,
-  height = iconBaseDefaults.height,
-}: React.SVGProps<SVGSVGElement> & {
-  fill?: string;
-  width?: string;
-  height?: string;
-}) => (
+export const Star = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width={width}
-    height={height}
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
-    fill={fill}
+    fill={`var(${props.fill}, #000)`}
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <g clipPath="url(#clip0_2066_3092)">
       <path
         d="M12 6.75357L12.9307 9.61787L13.38 11H17.8451L15.4085 12.77L14.2329 13.6241L14.6819 15.0061L15.6126 17.8705L13.1755 16.1L12 15.2462L10.8245 16.1L8.3878 17.87L9.3185 15.0056L9.7675 13.6236L8.5919 12.77L6.1553 11H10.62L11.0689 9.61797L12 6.75357ZM12 0.281372L9.1672 8.99997H0L7.4164 14.3883L4.5836 23.1068L12 17.7184L19.4164 23.1068L16.5836 14.3882L24 8.99997H14.8328L12 0.281372Z"
-        fill="black"
+        fill={`var(${props.fill}, #000)`}
       />
     </g>
     <defs>
       <clipPath id="clip0_2066_3092">
-        <rect width={width} height={height} fill="white" />
+        <rect width="24" height="24" fill={`var(${props.fill}, #000)`} />
       </clipPath>
     </defs>
   </svg>
