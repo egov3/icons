73f6d9c46a7fa06a1e9dc3845dd1ba68cc873051
Zhassulan-Baigozha <zhassulanbaigozha@gmail.com>
2025-02-02 15:30:35 +0500

    feat: [egov-9450] configure storybook (#8)
    * feat: [egov-9450] configured storybook
    
    * feat: [egov-9450] migration
    
    * feat: [egov-9450] updated readme.md
    
    ---------
    
    Co-authored-by: Zhassulan <zhassulan_baigozha@epam.com>

---
+++ .storybook/main.tsx
@@ -0,0 +1,23 @@
+// .storybook/main.tsx
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+    "@storybook/addon-webpack5-compiler-babel",
+    "@chromatic-com/storybook",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {},
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+
+export default config;
