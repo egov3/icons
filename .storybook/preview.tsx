7bae5362f5efd62e24a75ebec788b9908c15d5e3
Zhassulan <zhassulan_baigozha@epam.com>
2025-02-02 14:40:22 +0500

    feat: [egov-9450] configured storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,17 @@
+// .storybook/preview.tsx
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: {},
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export default preview;
