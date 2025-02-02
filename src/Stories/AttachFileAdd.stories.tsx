d307d8fc6dc7e6fee797d894868abded83de7d42
Zhassulan <zhassulan_baigozha@epam.com>
2025-02-02 14:44:15 +0500

    feat: [egov-9450] migration

--- src/Stories/AttachFileAdd.stories.tsx
+++ src/Stories/AttachFileAdd.stories.tsx
@@ -4,22 +4,19 @@ import React, { useState } from "react";
 import { AttachFileAdd } from "../Additional/AttachFileAdd";
 import { CardWrapperItem } from "./CardWrapperItem";
 
-const DefaultAccordionComponent = () => {
-  const [open, setOpen] = useState(true);
-  return (
-    <CardWrapperItem>
-      <div
-        style={{
-          borderRadius: "12px",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <AttachFileAdd />
-      </div>
-    </CardWrapperItem>
-  );
-};
+const DefaultAccordionComponent = () => (
+  <CardWrapperItem>
+    <div
+      style={{
+        borderRadius: "12px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <AttachFileAdd />
+    </div>
+  </CardWrapperItem>
+);
 
 export const AccordionStory = () => <DefaultAccordionComponent />;
 
